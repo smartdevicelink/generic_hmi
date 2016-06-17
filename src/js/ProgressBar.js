177447059e4898fe829191ff2c6bc1afba449288
Gabe Kelley <hello@gabekelley.com>
2016-06-17 13:28:24 -0400

    implemented progress bar

--- src/js/ProgressBar.js
+++ src/js/ProgressBar.js
@@ -3,12 +3,20 @@ import React from 'react';
 export default class ProgressBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            progress: 75
+        };
     }
 
     render() {
+
+        let progress = {
+            width: this.state.progress + "%"
+        }
+
         return (
             <div className="progress-bar">
-                <div className="progress-bar__progress"></div>
+                <div className="progress-bar__progress" style={ progress }></div>
             </div>
         )
     }
