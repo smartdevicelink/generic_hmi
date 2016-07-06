a53546288eb6270ea30269302b74d8f3d79ccd3e
Justin Dickow <jjdickow@gmail.com>
2016-07-06 14:58:59 -0400

    help

--- src/js/ControlBar.js
+++ src/js/ControlBar.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import ControlBarItem from './ControlBarItem'
+import iconSeekLeft from '../img/icons/icon-seek-left.svg';
+import iconSeekRight from '../img/icons/icon-seek-right.svg';
+import iconPlay from '../img/icons/icon-play.svg';
+import iconPlaylists from '../img/icons/icon-playlists.svg';
+import iconRepeat from '../img/icons/icon-repeat.svg';
 
-export default class ProgressBar extends React.Component {
+
+export default class ControlBar extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -8,11 +15,11 @@ export default class ProgressBar extends React.Component {
     render() {
         return (
             <div className="control-bar th-bg-color">
-                <div className="control-bar__control th-b-color-secondary"></div>
-                <div className="control-bar__control th-b-color-secondary"></div>
-                <div className="control-bar__control th-b-color-secondary"></div>
-                <div className="control-bar__control th-b-color-secondary"></div>
-                <div className="control-bar__control th-b-color-secondary"></div>
+                <ControlBarItem icon={iconRepeat}/>
+                <ControlBarItem icon={iconSeekLeft}/>
+                <ControlBarItem icon={iconPlay}/>
+                <ControlBarItem icon={iconSeekRight}/>
+                <ControlBarItem icon={iconPlaylists}/>
             </div>
         )
     }
