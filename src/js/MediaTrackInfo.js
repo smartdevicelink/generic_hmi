85c98bb2b85feab2e2936720ab0b2e6a61d394ab
justin <justin@livio.io>
2016-07-29 07:17:47 -0700

    Fix timer not resuming after pause when COUNTUP is used

--- src/js/MediaTrackInfo.js
+++ src/js/MediaTrackInfo.js
@@ -20,6 +20,7 @@ export default class MediaTrackInfo extends React.Component {
                 clearInterval(this.interval)
                 break        
             case "RESUME":
+            case "COUNTUP":
                 clearInterval(this.interval)
                 this.interval = setInterval(this.forceUpdate.bind(this), 1000)
                 break
