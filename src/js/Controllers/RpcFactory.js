f0c8db7297ca6cc3cbb5a69608eeeab691e111b1
justin <justin@livio.io>
2016-08-16 10:12:23 -0700

    copy sdl_hmi button capabilities for now

--- src/js/Controllers/RpcFactory.js
+++ src/js/Controllers/RpcFactory.js
@@ -43,26 +43,27 @@ class RpcFactory {
             "result": {
                 "method": rpc.method,
                 "code": 0,
-                "capabilities": [
-                    {
-                        "name": "OK",
-                        "shortPressAvailable": true,
-                        "longPressAvailable": false,
-                        "upDownAvailable": false
-                    },
-                    {
-                        "name": "SEEKLEFT",
-                        "shortPressAvailable": true,
-                        "longPressAvailable": false,
-                        "upDownAvailable": false
-                    },
-                    {
-                        "name": "SEEKRIGHT",
-                        "shortPressAvailable": true,
-                        "longPressAvailable": false,
-                        "upDownAvailable": false
-                    }
-                ]
+                // "capabilities": [
+                //     {
+                //         "name": "OK",
+                //         "shortPressAvailable": true,
+                //         "longPressAvailable": false,
+                //         "upDownAvailable": false
+                //     },
+                //     {
+                //         "name": "SEEKLEFT",
+                //         "shortPressAvailable": true,
+                //         "longPressAvailable": false,
+                //         "upDownAvailable": false
+                //     },
+                //     {
+                //         "name": "SEEKRIGHT",
+                //         "shortPressAvailable": true,
+                //         "longPressAvailable": false,
+                //         "upDownAvailable": false
+                //     }
+                // ]
+                "capabilities":[{"name":"PRESET_0","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_1","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_2","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_3","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_4","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_5","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_6","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_7","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_8","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"PRESET_9","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"OK","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"SEEKLEFT","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"SEEKRIGHT","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"TUNEUP","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true},{"name":"TUNEDOWN","shortPressAvailable":true,"longPressAvailable":true,"upDownAvailable":true}],"presetBankCapabilities":{"onScreenPresetsAvailable":true}
             }
         })
     }
