cfe671826ee44289e471f5e4c1ab02b6a6bd864d
JackLivio <jack@livio.io>
2017-12-05 13:58:50 -0500

    In App Menu Fixes
    App will return to appropriate template after user makes a command selection. Also the user has the option to not make any menu selection and return back to the app view by pressing the menu button again.

--- src/js/AppIcon.js
+++ src/js/AppIcon.js
@@ -9,12 +9,14 @@ export default class AppIcon extends React.Component {
     }
 
     render() {
+        //While viewing menu, pressing menu button takes user back to app screen
+        var path = this.props.isShowingMenu ? this.props.backLink : "/inappmenu" 
         var icon = this.props.icon ?
             (<img className="app-icon" src={this.props.icon} />)
             : (<span className="app-icon"></span>)
         return (
             <div>
-                <Link to="/inappmenu" onClick={() => this.props.onSelection(this.props.appID)}>
+                <Link to={path} onClick={() => this.props.onSelection(this.props.appID, path)}>
                     {icon}
                     <span className="svg-wrap" dangerouslySetInnerHTML={{__html: iconMenu}} />
                 </Link>
