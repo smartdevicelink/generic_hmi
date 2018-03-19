55d2c3cfdc19a8b37f8fbbb2afeb216488567dc2
JackLivio <jack@livio.io>
2018-03-19 14:25:26 -0400

    TextButtons With Graphic Template

--- src/js/entry.js
+++ src/js/entry.js
@@ -6,6 +6,7 @@ import MediaPlayer from './MediaPlayer';
 import NonMedia from './Templates/NonMedia/NonMedia'
 import LargeGraphicOnly from './Templates/LargeGraphicOnly/LargeGraphicOnly'
 import LargeGraphicWithSoftbuttons from './Templates/LargeGraphicWithSoftbuttons/LargeGraphicWithSoftbuttons'
+import TextButtonsWithGraphic from './Templates/TextButtonsWithGraphic/TextButtonsWithGraphic'
 import HMIMenu from './HMIMenu';
 import InAppMenu from './InAppMenu';
 import InAppList from './InAppList';
@@ -73,6 +74,7 @@ ReactDOM.render((
             <Route path="/nonmedia" component={NonMedia} />
             <Route path="/large-graphic-only" component={LargeGraphicOnly} />
             <Route path="/large-graphic-with-softbuttons" component={LargeGraphicWithSoftbuttons} />
+            <Route path="/text-buttons-with-graphic" component={TextButtonsWithGraphic} />
             <Route path="/inappmenu" component={InAppMenu} />
             <Route path="/inapplist" component={InAppList} />
             <Route path="/tilesonly" component={TilesOnly} />
--- src/js/reducers.js
+++ src/js/reducers.js
@@ -260,6 +260,10 @@ function ui(state = {}, action) {
                     break
                 case "LARGE_GRAPHIC_WITH_SOFTBUTTONS":
                     app.displayLayout = "large-graphic-with-softbuttons"
+                    break
+                case "TEXTBUTTONS_WITH_GRAPHIC":
+                    app.displayLayout = "text-buttons-with-graphic"
+                    break
                 default: 
                     break
             }
