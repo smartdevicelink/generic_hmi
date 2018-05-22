9b7924a03f6241e3dbd24d1e9d0f32caf04fbca1
JackLivio <jack@livio.io>
2018-05-22 10:44:04 -0400

    Fix Indentation

--- src/js/ControlBarItem.js
+++ src/js/ControlBarItem.js
@@ -15,8 +15,8 @@ export default class ControlBar extends React.Component {
             )            
         } else {
             var image = this.props.icon ?
-            <span key="icon" className="svg-wrap" dangerouslySetInnerHTML={{__html: this.props.icon}} /> :
-            <img key="image" src={this.props.image} />
+                <span key="icon" className="svg-wrap" dangerouslySetInnerHTML={{__html: this.props.icon}} /> :
+                <img key="image" src={this.props.image} />
             return (
                 <div
                     className={`control-bar__control th-b-color-secondary control-bar__control--${ this.props.class }`}
