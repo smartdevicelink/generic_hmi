96b65685057d114004f817680b36d92eb05a73da
JackLivio <jack@livio.io>
2018-06-13 14:51:13 -0400

    Add ability to play tts audio files and text

--- src/js/Controllers/TTSController.js
+++ src/js/Controllers/TTSController.js
@@ -2,10 +2,69 @@ import RpcFactory from './RpcFactory'
 class TTSController {
     constructor () {
         this.addListener = this.addListener.bind(this)
+        this.audioPlayer = new Audio();
+        this.filePlaylist = [];
     }
     addListener(listener) {
         this.listener = listener
     }
+
+    playAudio() {
+        if(this.filePlaylist.length == 0) {
+            this.audioPlayer.onended = null;
+            if(!this.audioPlayer.paused) {
+                this.audioPlayer.pause();
+                this.audioPlayer.src = "";
+                return;
+            }
+        }
+
+        var path = this.filePlaylist[0].text;
+        this.filePlaylist.shift();
+
+        this.audioPlayer.onended = () => {
+            this.audioPlayer.src ="";
+            if(this.filePlaylist[0].type === "FILE") {
+                this.playAudio();
+            } else if (this.filePlaylist[0].type === "TEXT"){
+                this.speak();
+            }
+        }
+
+        this.audioPlayer.src = path;
+        this.audioPlayer.play();
+    }
+
+    speak() {
+        if(this.filePlaylist.length == 0) {
+            return;
+        }
+
+        var text = this.filePlaylist[0].text;
+        this.filePlaylist.shift();
+
+        var speechPlayer = new SpeechSynthesisUtterance();
+
+        speechPlayer.onend = () => {
+            if(this.filePlaylist[0].type === "FILE") {
+                this.playAudio();
+            } else if (this.filePlaylist[0].type === "TEXT"){
+                this.speak();
+            }            
+        }
+
+        speechPlayer.onerror = (event) => {
+            console.log("Text to speech error. Make sure your browser supports SpeechSynthesisUtterance");
+        }
+
+        speechPlayer.text = text;
+        speechPlayer.volume = 1;
+        speechPlayer.rate = 1;
+        speechPlayer.pitch = 0;
+        window.speechSynthesis.speak(speechPlayer)
+
+    }
+    
     handleRPC(rpc) {
         let methodName = rpc.method.split(".")[1]
         switch(methodName) {
@@ -18,6 +77,20 @@ class TTSController {
             case "SetGlobalProperties":
                 return true
             case "Speak":
+                var ttsChunks = rpc.params.ttsChunks
+                this.filePlaylist = []
+                for (var i=0; i<ttsChunks.length; i++) {
+                        this.filePlaylist.push(ttsChunks[i])
+                }
+
+                if(this.filePlaylist.length > 0) {
+                    if(this.filePlaylist[0].type === "FILE") {
+                        this.playAudio();
+                    } else if (this.filePlaylist[0].type === "TEXT"){
+                        this.speak();
+                    }
+                }
+                
                 return true;
         }
     }
