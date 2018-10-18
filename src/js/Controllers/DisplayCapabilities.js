897a9cdda932fdf78fcbf3e01c2902da8207a09f
JackLivio <jack@livio.io>
2018-10-18 17:15:47 -0400

    Add tiles only to capabilities

--- src/js/Controllers/DisplayCapabilities.js
+++ src/js/Controllers/DisplayCapabilities.js
@@ -1931,6 +1931,198 @@ let capabilities = {
 				"imageSupported": true
 			}
 		]
+	},
+	"TILES_ONLY": {
+		"displayCapabilities": {
+			"displayType": "GEN2_8_DMA",
+			"displayName": "GENERIC_DISPLAY",
+			"textFields": [{
+					"name": "alertText1",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "alertText2",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "alertText3",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "menuName",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "secondaryText",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "tertiaryText",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				},
+				{
+					"name": "menuTitle",
+					"characterSet": "TYPE2SET",
+					"width": 500,
+					"rows": 1
+				}
+			],
+			"imageFields": [{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "softButtonImage",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				},
+				{
+					"name": "menuIcon",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 40,
+						"resolutionHeight": 40
+					}
+				},
+				{
+					"name": "cmdIcon",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 150,
+						"resolutionHeight": 150
+					}
+				},
+				{
+					"name": "appIcon",
+					"imageTypeSupported": ["GRAPHIC_PNG"],
+					"imageResolution": {
+						"resolutionWidth": 50,
+						"resolutionHeight": 50
+					}
+				}
+			],
+			"mediaClockFormats": [],
+			"graphicSupported": true,
+			"templatesAvailable": [
+				"DEFAULT", "MEDIA", "NON-MEDIA", "LARGE_GRAPHIC_WITH_SOFTBUTTONS", "LARGE_GRAPHIC_ONLY",
+				"GRAPHIC_WITH_TEXTBUTTONS", "TEXTBUTTONS_WITH_GRAPHIC", "TEXTBUTTONS_ONLY",
+				"TEXT_WITH_GRAPHIC", "GRAPHIC_WITH_TEXT", "DOUBLE_GRAPHIC_WITH_SOFTBUTTONS"
+			],
+			"screenParams": {
+				"resolution": {
+					"resolutionWidth": 960,
+					"resolutionHeight": 675
+				},
+				"touchEventAvailable": {
+					"pressAvailable": true,
+					"multiTouchAvailable": false,
+					"doublePressAvailable": false
+				}
+			},
+			"imageCapabilities": ["DYNAMIC", "STATIC"]
+		},
+		"hmiZoneCapabilities": "FRONT",
+		"hmiCapabilities": {
+			"navigation": false,
+			"phoneCall": false
+		},
+		"systemCapabilities": {
+			"navigationCapability": {
+				"sendLocationEnabled": false,
+				"getWayPointsEnabled": false
+			},
+			"phoneCapability": {
+				"dialNumberEnabled": false
+			}
+		},
+			"softButtonCapabilities": [{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			},
+			{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			},
+			{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			},
+			{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			},
+			{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			},
+			{
+				"shortPressAvailable": true,
+				"longPressAvailable": false,
+				"upDownAvailable": false,
+				"imageSupported": true
+			}
+		]
 	}
 }
 
