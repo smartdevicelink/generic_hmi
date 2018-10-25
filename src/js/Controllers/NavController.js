b87ef4944b8234915a43d1d4a062c828861fdb33
JackLivio <jack@livio.io>
2018-10-25 10:51:17 -0400

    Add isReady, Audio, and default responses for nav controller

--- src/js/Controllers/NavController.js
+++ src/js/Controllers/NavController.js
@@ -10,11 +10,19 @@ class NavController {
     handleRPC(rpc) {
         let methodName = rpc.method.split(".")[1]
         switch (methodName) {
+            case "IsReady":
+                return {"rpc": RpcFactory.IsReadyResponse(rpc, false)}
             case "StartStream":
                 var message = "This system does not support video streaming."
                 return {"rpc": RpcFactory.UnsupportedResourceResponse(rpc, message)};
+            case "StartAudioStream":
+                var message = "This system does not support audio streaming."
+                return {"rpc": RpcFactory.UnsupportedResourceResponse(rpc, message)};
             case "SetVideoConfig":
                 var message = "This system does not support video streaming."
+                return {"rpc": RpcFactory.UnsupportedResourceResponse(rpc, message)};
+            default:
+                var message = "This RPC is not supported."
                 return {"rpc": RpcFactory.UnsupportedResourceResponse(rpc, message)};
         }
     }
