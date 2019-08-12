245da723d23fb0c58452ed9ac96b1cefbdaf0552
Shobhit Adlakha <adlakhashobhit@gmail.com>
2019-08-12 10:33:59 -0400

    Use optional header for deviceName instead of having a devicename element for all HScrollMenuItems

--- src/js/HScrollMenuItem.js
+++ src/js/HScrollMenuItem.js
@@ -10,18 +10,23 @@ export default class HScrollMenuItem extends React.Component {
         const graphic = this.props.menuItem.class == 'with-icon' ?
         (<span className="svg-wrap" dangerouslySetInnerHTML={{__html: `${menuItem.image}`}} />)
         : <img src={menuItem.image} /> ;
+
+        var header = menuItem.devicename;
+        var header_html = (header) ? 
+            (<div className="hscrollmenu-item__header">
+                <p className="t-light th-f-color-secondary t-oneline">{ header }</p>
+            </div>)
+            : null;
         return (
             <div
                 className={`hscrollmenu-item th-b-color th-tile-background-color ${menuItem.class}`}
                 onClick={() => this.props.onSelection(this.props.appID, this.props.cmdID, this.props.menuID)}>
-                <div className="hscrollmenu-item__device-name">
-                    <p className="t-light th-f-color-secondary t-oneline">{ menuItem.devicename }</p>
-                </div>
+                { header_html }
                 <div className="hscrollmenu-item__image">
                     { graphic }
                 </div>
                 <div className="hscrollmenu-item__name">
-                    <p className="t-small t-light th-f-color">{menuItem.name}</p>
+                    <p className="t-small t-light th-f-color">{ menuItem.name }</p>
                 </div>
             </div>
         )
