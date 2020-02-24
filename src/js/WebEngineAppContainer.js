65010bd148f137d1c4b1890dfc079e7a6b4de3bb
collin <collin@livio.io>
2020-02-24 14:28:52 -0500

    fixup! initial web engine support
    remove p tag from active web engine app containers

--- src/js/WebEngineAppContainer.js
+++ src/js/WebEngineAppContainer.js
@@ -3,11 +3,7 @@ import React from 'react';
 export default class WebEngineAppContainer extends React.Component {
     render() {
         return (<div className="webengine-app-container">
-            <p>{`app running: ${this.props.policyAppID}`}</p>
-            <iframe src={this.props.iframeUrl} 
-                style={{
-                    display: 'none'
-                }}/>
+            <iframe src={this.props.iframeUrl} style={{ display: 'none' }} />
         </div>);
     }
 }
