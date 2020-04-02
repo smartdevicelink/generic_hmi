001f7334cf04bf7e6c20a2502337a35bc3cea9ee
collin <collin@livio.io>
2020-04-02 12:58:05 -0400

    move vscrollmenuitem icon to the left side

--- src/js/VScrollMenuItem.js
+++ src/js/VScrollMenuItem.js
@@ -13,19 +13,17 @@ export default class VScrollMenuItem extends React.Component {
                 to={this.props.menuItem.link}
                 className="vscrollmenu-item th-b-color th-bb-color-secondary"
                 onClick={() => this.props.onSelection(this.props.appID, this.props.cmdID, this.props.menuID, this.props.isPerformingInteraction, this.props.interactionId)}>
-                <div className="vscrollmenu-item__name">
-                    <div className="t-large t-light th-f-color vscrollmenu-item-primary">
-                        {this.props.menuItem.name}
-                        <SoftButtonImage class="vscrollmenu-item-image" image={this.props.image ? this.props.image : null} 
-                            imageType={this.props.imageType ? this.props.imageType : null}
-                            isTemplate={this.props.isTemplate ? this.props.isTemplate : null}
-                            theme={this.props.theme}
-                        />   
+                <div className="vscrollmenu-item__primary">
+                    <SoftButtonImage class="vscrollmenu-item__image" image={this.props.image ? this.props.image : null} 
+                        imageType={this.props.imageType ? this.props.imageType : null}
+                        isTemplate={this.props.isTemplate ? this.props.isTemplate : null}
+                        theme={this.props.theme}
+                    />
+                    <div className="vscrollmenu-item__name">
+                        <p className="t-large t-light th-f-color">{this.props.menuItem.name}</p>
+                        <p className="t-large t-light th-f-color-secondary">{this.props.menuItem.info}</p>
                     </div>
-                    <p className="t-large t-light th-f-color-secondary">{this.props.menuItem.info}</p>
-                </div>
-                <div>
-                    <span className="svg-wrap" dangerouslySetInnerHTML={{__html: iconArrowRight}} />
+                    <span className="vscrollmenu-item__arrow svg-wrap" dangerouslySetInnerHTML={{__html: iconArrowRight}} />
                 </div>
             </Link>
         )
