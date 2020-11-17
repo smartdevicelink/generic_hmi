336c6278494f202f5942909f9dcb6a5976b93a6f
Christopher Rokita <chris@livio.io>
2020-11-17 14:47:45 -0500

    Test PTU OFF

--- public/Flags.js
+++ public/Flags.js
@@ -2,7 +2,7 @@ window.flags = {
     ExternalPolicies: false,
     ExternalPoliciesPackUrl: 'ws://127.0.0.1:8088',
     ExternalPoliciesUnpackUrl: 'ws://127.0.0.1:8089',
-    PTUWithModemEnabled : true,
+    PTUWithModemEnabled : false,
     FileSystemApiUrl: 'ws://127.0.0.1:8081',
     CoreHost: '127.0.0.1',
     CorePort: 8087,
