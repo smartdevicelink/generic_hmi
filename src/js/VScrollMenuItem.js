036539bcdfdd655611852a75190e6b979f520b30
JackLivio <jack@livio.io>
2021-01-06 15:07:21 -0500

    Line break

--- src/js/VScrollMenuItem.js
+++ src/js/VScrollMenuItem.js
@@ -21,7 +21,8 @@ export default class VScrollMenuItem extends React.Component {
             "Driver Distraction Disabled" : this.props.menuItem.info ? 
             this.props.menuItem.info : this.props.menuItem.secondaryText;
         
-        var tertiaryText = this.props.enabled === false ? "" : this.props.menuItem.tertiaryText;
+        var tertiaryText = this.props.enabled === false ? 
+            "" : this.props.menuItem.tertiaryText;
 
         return (
             <Link
