caa3badbb07db547bdaa493e5ffcb5f835932c80
Shobhit Adlakha <ShobhitAd@users.noreply.github.com>
2021-05-19 15:27:30 -0400

    Apply suggestions from code review
    Co-authored-by: Collin <iCollin@users.noreply.github.com>

--- src/js/Controllers/RpcFactory.js
+++ src/js/Controllers/RpcFactory.js
@@ -258,7 +258,7 @@ class RpcFactory {
                 "code": 0
             }
         }
-        if (choiceID || choiceID === 0) { //Keyboard PI does not have a choice id response
+        if ('number' === typeof choiceID) { //Keyboard PI does not have a choice id response
             msg.result["choiceID"] = choiceID;
         }
         if (manualTextEntry) {
