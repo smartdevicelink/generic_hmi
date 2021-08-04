7917afe20cdcd5eafbd6ff717e0815235f311ad9
valerii <valera.malkof@gmail.com>
2021-08-04 13:50:35 +0300

    SDL-0189 Revert file changes in Alert.js

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,58 +1,18 @@
 import React from 'react';
 
-import { AlertStrings } from './containers/Metadata'
-import { AlertButtons } from './containers/Buttons';
+import {AlertStrings} from './containers/Metadata'
+import {AlertButtons} from './containers/Buttons';
 import Image from './Templates/Shared/Image'
 import StaticIcon from './Templates/Shared/StaticIcon'
-import store from './store.js'
-import UIController from './Controllers/UIController'
 
-
-const OUT_OF_BOUND_RESET_PERIOD = 1000;
 export default class Alert extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            alertCounter: 5,
-            ifSpeak: true,
-            speakCounter: 5,
-            ttsStoped: false
-        }
-        this.pressResetTimeoutButton = this.pressResetTimeoutButton.bind(this);
-    }
-
-    pressResetTimeoutButton(event) {
-        let count = store.getState().system.resetPeriod / 1000;
-        if (count > OUT_OF_BOUND_RESET_PERIOD/1000) {
-            this.setState({ alertCounter: count });
-            UIController.resetAlertTimeout();
-            return;
-        }
-    }
-    changeCounter() {
-        this.setState(prevState => ({ alertCounter: prevState.alertCounter > 0 ? prevState.alertCounter - 1 : '' }))
-        this.setState(prevState => ({ speakCounter: prevState.speakCounter > 0 ? prevState.speakCounter - 1 : '' }))
-    }
-    componentDidMount() {
-        const { activeApp, ui } = store.getState()
-
-        if (activeApp) {
-            this.setState({alertCounter: ui[activeApp].alert.duration/1000});
-        }
-        this.interval = setInterval(() => this.changeCounter(), 1000);
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-
     render() {
         var fill = this.props.theme ? "#FFFFFF" : "#000000";
         var icon = this.props.icon ? this.props.icon : { imageType: "STATIC", value: "0xFE" }
 
         var iconElement = (icon.imageType === "STATIC")
-            ? (<StaticIcon class="alert-icon" image={icon.value} />)
-            : (<div className="alert-icon"><Image class="icon" image={icon.value} isTemplate={icon.isTemplate} fillColor={fill} /></div>);
+                 ? (<StaticIcon class="alert-icon" image={icon.value} />)
+                 : (<div className="alert-icon"><Image class="icon" image={icon.value} isTemplate={icon.isTemplate} fillColor={fill} /></div>);
 
         return (
             <div className="alert">
@@ -62,12 +22,11 @@ export default class Alert extends React.Component {
                     </p>
                 </div>
                 <div className="alert-top">
-                    <AlertStrings />
-                    {iconElement}
+                    <AlertStrings/>
+                    { iconElement }
                 </div>
-                <AlertButtons classPrefix="alert" />
+                <AlertButtons classPrefix="alert"/>
             </div>
         )
     }
 }
-
