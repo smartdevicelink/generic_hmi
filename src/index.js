2392633ea70538d73d883345f8d204a68c99ddaf
Christopher Rokita <chris@livio.io>
2021-11-10 12:56:47 -0500

    Remove bottom bar of UI

--- src/index.js
+++ src/index.js
@@ -220,19 +220,6 @@ class HMIApp extends React.Component {
                         {this.props.children}
                     </div>
                 </div>
-                <div>
-                    <div className="toggle-button" onClick={this.handleClick}>Toggle theme</div>
-                    <div className="shutdown-button" onClick={this.handleShutdown}>Shutdown</div>
-                    <div className="toggle-ptu-with-modem-button" >
-                        <input type="checkbox" onClick={this.togglePTUWithModem} checked={this.props.ptuWithModemEnabled}/>
-                        <label>PTU using in-vehicle modem</label>
-                    </div>
-                    <div className="driver-distraction-button" >
-                        <input type="checkbox" onClick={this.handleDDToggle} checked={this.props.dd}/>
-                        <label>Driver Distraction</label>
-                    </div>
-                    { resolutionSelector }
-                </div>
                 <video id="navi_stream" style={videoStyle} src={this.props.videoStreamUrl}
                     onTouchStart={this.onTouchBegin} onMouseDown={this.onTouchBegin}
                     onTouchMove={this.onTouchMove} onMouseMove={this.onTouchMove}
